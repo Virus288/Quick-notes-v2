9a74fd5a016d13bd05bc5633c8c614444212710d
Virus288 <pou20@wp.pl>
2020-10-31 15:12:44 +0100

    Saving notes as object instead of array. Fixed bug which was deleting notes from time to time

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.notes');
 const search = document.querySelector('.search input');
-let notes = [];
 const cookie = '<li class="list-group-item d-flex justify-content-between align-items-center"> <span>Hi there user. <br>It seems like its your first time here. Before we start, by using this app, you agree on colecting cookies</span> <i class="far fa-trash-alt delete"></i> </li>'
+let notes = {};
 
-// Add stuff
-const generateTemplate = notes => {
+// Add note
+const generateTemplate = (notes, id) => {
 
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${notes}</span>
-        <i id="${notes}" class="far fa-trash-alt delete"></i>
+        <i id="${id}" class="far fa-trash-alt delete"></i>
     </li>
     `;
 
@@ -18,28 +18,44 @@ const generateTemplate = notes => {
 
 };
 
+// Check for keys and values
+const key = (data) => {
+    let keys = Object.keys(notes);
+    let preId;
+    let id;
+    if(keys == NaN || keys.length == 0) {
+        preId = 1;
+    } else {
+        id = keys[keys.length -1];
+        preId = parseInt(id) + 1;
+    }
+    notes[preId] = data;
+    generateTemplate(data, preId);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    notesLeft();
+}
+
 // Check for submit on "Add new notes"
 addForm.addEventListener('submit', e => {
 
     e.preventDefault();
-    const data = addForm.add.value.trim();
+    let data = addForm.add.value.trim();
+    data = data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     
     if(data.length){
-        generateTemplate(data);
+        key(data)
         addForm.reset();
-        notes.push(data);
-        localStorage.setItem('notes', JSON.stringify(notes));
-        notesLeft()
     }
-
 });
 
 // Delete notes
 list.addEventListener('click', e => {
+    let keys = Object.keys(notes);
+
     if(e.target.classList.contains('delete')){
         e.target.parentElement.classList.toggle('deleteItem');
         setTimeout(() => {e.target.parentElement.remove()}, 1000);
-        notes = notes.filter(note => note !== e.target.id);
+        delete notes[e.target.id]
         localStorage.setItem('notes', JSON.stringify(notes));
         setTimeout(() => {notesLeft()}, 1000);
     }
@@ -48,42 +64,51 @@ list.addEventListener('click', e => {
 // Check if there are any notes left
 const notesLeft = () => {
     if(list.childElementCount == 0){
-        list.innerHTML = "<h4 class='text-dark text-center'>You have 0 notes</h4>";
-    } else {
+        list.innerHTML = "<h4 class='NoNotes text-dark text-center'>You have 0 notes</h4>";
+    } else if(list.childElementCount > 1 && list.contains(document.querySelector('.NoNotes'))) {
         list.removeChild(list.childNodes[0]);
     }
 }
 
 // Filter notes
-const filternotes = (term) => {
+const filternotes = (data) => {
     
     Array.from(list.children)
-    .filter((notes) => !notes.textContent.toLowerCase().includes(term))
+    .filter((notes) => !notes.textContent.toLowerCase().includes(data))
     .forEach((notes) => notes.classList.add('filtered'));
 
     Array.from(list.children)
-    .filter((notes) => notes.textContent.toLowerCase().includes(term))
+    .filter((notes) => notes.textContent.toLowerCase().includes(data))
     .forEach((notes) => notes.classList.remove('filtered'));
 };
 
 // Search keyup
 search.addEventListener('keyup', () => {
-    const term = search.value.trim().toLowerCase();
-    filternotes(term);
+    const data = search.value.trim().toLowerCase();
+    filternotes(data);
 });
 
-if(localStorage.getItem("cookies") !== "agreed"){
-    list.innerHTML += cookie;
-    localStorage.setItem('cookies', "agreed");
-}
+// Initial setup
+const init = () => {
+    if(localStorage.getItem("cookies") !== "agreed"){
+        list.innerHTML += cookie;
+        localStorage.setItem('cookies', "agreed");
+    }
+    
+    if(localStorage.getItem('notes') === null || localStorage.getItem('notes').length == 2){
+        console.log("No notes");
+        notesLeft();
+    } else {
+        localData = localStorage.getItem('notes');
+        notes = JSON.parse(localData);
+        let keys = Object.keys(notes);
+        let id = keys[keys.length -1];
 
-if(localStorage.getItem('notes').length == 2){
-    console.log("No notes");
-    notesLeft();
-} else {
-    localData = localStorage.getItem('notes');
-    notes = JSON.parse(localData);
-    notes.forEach(note => {
-        generateTemplate(note);
-    })
-}
+        keys.forEach(key => {
+            generateTemplate(notes[key], key);
+        })
+        
+    };
+};
+
+window.onload = init()
